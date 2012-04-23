b0128bc354c6bfa253d88a43af67ba013da51aa1
Christopher Lemire <christopher.lemire@gmail.com>
2012-04-23 05:30:18 -0500

    Added TODO comments

--- src/jspeak/Voice.java
+++ src/jspeak/Voice.java
@@ -12,8 +12,11 @@ import java.io.File;
  * @author Christopher Lemire <christopher.lemire@gmail.com>
  */
 public class Voice {
+  private File f;
   public Voice() {
-    File f = new File("/usr/share/espeak-data/voices/mbrola");
+    //TODO Read http://docs.oracle.com/javase/tutorial/essential/io/dirs.html
+    //TODO Read http://docs.oracle.com/javase/tutorial/essential/io/find.html
+    f = new File("/usr/share/espeak-data/voices/mbrola");
     //TODO Get voices and store as an array
     f = new File("/usr/share/mbrola");
     //TODO Get voices and add to the array
