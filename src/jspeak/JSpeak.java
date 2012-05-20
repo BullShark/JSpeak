7ae30277c587840cb407cb2cfa40c2360476435c
Christopher Lemire <christopher.lemire@gmail.com>
2012-05-20 02:08:13 -0500

    Comments

--- src/jspeak/JSpeak.java
+++ src/jspeak/JSpeak.java
@@ -361,8 +361,11 @@ public class JSpeak extends JPanel
             + "Feedback is appreciated!\n\n"
             + "For command output and error use -g or --debug\n");
 
-    //Schedule a job for the event dispatch thread:
-    //creating and showing this application's GUI.
+    /*
+     * Schedule a job for the event dispatch thread:
+     * 
+     * Creating and showing this application's GUI.
+     */
     SwingUtilities.invokeLater(new Runnable() {
       public void run() { createAndShowGUI(); }
     });
